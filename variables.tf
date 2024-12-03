803af0c29b59553843c07252e11a7cdaf5e977a3
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-12-03 17:52:02 +0100

    added access_key and secret_key

--- variables.tf
+++ variables.tf
@@ -1,5 +1,8 @@
 variable "region" {
   description = "the default region for this configuration"
-  type = string
-  default = "eu-north-1"
+  type        = string
+  default     = "eu-north-1"
 }
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
