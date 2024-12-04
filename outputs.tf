39906745f230637f4448749ff421df2cc2f8f5a2
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-12-04 04:09:28 +0100

    deleted outputs that were not needed

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,6 @@ output "s3_bucket_name" {
   value = aws_s3_bucket.tfstate.bucket
 }
 
-output "s3_bucket_arn" {
-  # The Amazon Resource Name (ARN) for the S3 bucket. This can be helpful when referencing the bucket in IAM policies, logging configurations, or for any service requiring an ARN.
-  value = aws_s3_bucket.tfstate.arn
-}
-
 output "s3_bucket_id" {
   # The unique ID for the S3 bucket, which is useful if you need to reference the bucket programmatically or in other parts of your infrastructure.
   value = aws_s3_bucket.tfstate.id
@@ -18,11 +13,6 @@ output "s3_bucket_domain_name" {
   value = aws_s3_bucket.tfstate.bucket_domain_name
 }
 
-output "s3_bucket_region" {
-  # If you are using a multi-region setup, you might want to output the region of the S3 bucket to ensure the bucket is created in the correct region.
-  value = aws_s3_bucket.tfstate.region
-}
-
 
 
 output "dynamodb_table_name" {
@@ -30,21 +20,7 @@ output "dynamodb_table_name" {
   value = aws_dynamodb_table.tfstate_lock.name
 }
 
-output "dynamodb_table_arn" {
-  # The Amazon Resource Name (ARN) for the DynamoDB table. This is helpful if you need to reference the table in IAM policies or other AWS resources.
-  value = aws_dynamodb_table.tfstate_lock.arn
-}
-
 output "dynamodb_table_id" {
   # The unique ID of the DynamoDB table. This can be useful for tracking or logging purposes.
   value = aws_dynamodb_table.tfstate_lock.id
 }
-
-# Provisioned Throughput (if you are using provisioned billing mode)
-# Since this set_up is using PAY_PER_REQUEST, you don't need to output provisioned throughput settings, but if you use provisioned mode (PROVISIONED billing mode), you could output read and write capacity units like so:
-# output "dynamodb_read_capacity" {
-#   value = aws_dynamodb_table.tfstate_lock.provisioned_throughput[0].read_capacity_units
-# }
-# output "dynamodb_write_capacity" {
-#   value = aws_dynamodb_table.tfstate_lock.provisioned_throughput[0].write_capacity_units
-# }
